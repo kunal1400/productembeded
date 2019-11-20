06e771ba45de57fc85d8f2d6c25f3e6371082e40
Kunal Malviya <lucky.kunalmalviya@gmail.com>
2019-11-20 13:10:31 +0530

    Now data will come from feed

--- productembeded.php
+++ productembeded.php
@@ -3,13 +3,38 @@
 * Template Name: Product Embeded Page
 **/
 
-// require('./wp-load.php');
 wp_head();
-// echo do_shortcode("[productembeded ids='817,816']");
 if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post(); 
+		// echo do_shortcode("[productembeded]");
 		the_content();
 	}
 }
 wp_footer();
+
+
+?>
+
+<script type="text/javascript">
+	// var x = new XMLHttpRequest();
+	// x.open("GET", "http://localhost/wp-plugin-tasks/productembeded/", true);
+	// // x.setRequestHeader( 'Access-Control-Allow-Origin', '*');
+	// x.onreadystatechange = function () {
+	//   if (x.readyState == 4 && x.status == 200) {	    
+	//     console.log(doc);
+	//   }
+	// };
+	// x.onerror = function(XMLHttpRequest, textStatus, errorThrown) {
+	// 	console.log( 'The data failed to load :(' );
+	// 	console.log(JSON.stringify(XMLHttpRequest));
+	// };
+	// x.onload = function() {
+	// 	console.log('SUCCESS!');
+	// }
+	// x.send(null);
+
+	// $.post('https://topnakup.si/wp-content/uploads/woo-product-feed-pro/xml/7Oqy1OO12CzQTBGJc6QH5VlXBGUECyBT.xml', { url: url }, function(data) {
+	//     document.getElementById('somediv').innerHTML = data;        
+	// });
+</script>
--- woocommerce-product-embed.php
+++ woocommerce-product-embed.php
@@ -18,62 +18,107 @@ function productembeded_enqueue_script(){
 **/
 add_shortcode( 'productembeded', 'productembeded_shortcode_callback' );
 function productembeded_shortcode_callback( $atts ) {
+	add_filter('show_admin_bar', '__return_false');
+
 	// Get shortcodes
 	$a = shortcode_atts( array(
 		"ids" => "",
 	), $atts );
 
-	// If region code is set in url
-	if( !empty($a['ids']) ) {
-		$productIds = $a['ids'];		
-		$ids = explode(",", $productIds);
-		$ids = array_unique($ids);
-		$string = '<div class="container"><div class="row">';
-		foreach ($ids as $i => $id) {
-			// Get $product object from product ID source https://businessbloomer.com/woocommerce-easily-get-product-info-title-sku-desc-product-object/
-			$product = wc_get_product( $id );			
-			$string .= sendHtml($product);
-		}
-		$string .= '</div></div>';
-		return $string;
-	} else {
-		return;
+	$productFeedUrl = 'https://topnakup.si/wp-content/uploads/woo-product-feed-pro/xml/7Oqy1OO12CzQTBGJc6QH5VlXBGUECyBT.xml';
+	libxml_use_internal_errors(true);
+
+	$curl = curl_init();
+	curl_setopt_array($curl, Array(
+	    CURLOPT_URL            => $productFeedUrl,
+	    CURLOPT_RETURNTRANSFER => TRUE,
+	    CURLOPT_ENCODING       => 'UTF-8'
+	));
+	$data = curl_exec($curl);
+	curl_close($curl);
+	$xml = simplexml_load_string($data);
+
+	if ($xml === false) {
+	    echo "Failed loading XML\n";
+	    foreach(libxml_get_errors() as $error) {
+	        echo "\t", $error->message;
+	    }
+	} 
+	else {
+		$jsonEncode = json_encode($xml);
+		$products = json_decode($jsonEncode,true);
+		if(count($products['product']) > 0) {
+			$onlyProducts = $products['product'];
+			$string = '<div class="container"><div class="row">';
+			for ($i=0; $i < count($onlyProducts) ; $i++) { 
+				$data = $onlyProducts[$i];				
+				$string .= sendHtml($data);
+			}
+			return $string .= '</div></div>';
+		} else {
+			return "ddd";
+		}		
 	}
 }
 
-function sendHtml($product) {
-	// $product->get_image_id();
-	// $product->get_gallery_image_ids();
-	// $product->get_type();
-	// echo  get_option('woocommerce_currency');
-	// echo  get_woocommerce_currency_symbol("USD");
+// /**
+// * Adding shortcode for this plugin
+// **/
+// add_shortcode( 'productembeded', 'productembeded_shortcode_callback' );
+// function productembeded_shortcode_callback( $atts ) {
+// 	// Get shortcodes
+// 	$a = shortcode_atts( array(
+// 		"ids" => "",
+// 	), $atts );
 
-	$name = $product->get_name();
-	$image = $product->get_image();
-	$description = $product->get_short_description();
-	$price = $product->get_price();
-	$regularPrice = $product->get_regular_price();
-	$saleprice = $product->get_sale_price();
-	$is_on_sale = $product->is_on_sale();
+// 	// If region code is set in url
+// 	if( !empty($a['ids']) ) {
+// 		$productIds = $a['ids'];		
+// 		$ids = explode(",", $productIds);
+// 		$ids = array_unique($ids);
+// 		$string = '<div class="container"><div class="row">';
+// 		foreach ($ids as $i => $id) {
+// 			// Get $product object from product ID source https://businessbloomer.com/woocommerce-easily-get-product-info-title-sku-desc-product-object/
+// 			$product = wc_get_product( $id );			
+// 			$string .= sendHtml($product);
+// 		}
+// 		$string .= '</div></div>';
+// 		return $string;
+// 	} else {
+// 		return;
+// 	}
+// }
+
+function sendHtml($product) {	
+	$name = $product['title'];
+	$image = $product['image_link'];
+	$description = $product['description'];
+	$regularPrice = $product['price']+$product['discount'];
 	$symbol = get_woocommerce_currency_symbol();
-	$permalink = get_permalink( $product->get_id() );
+	$saleprice = $product['price'];
+	$is_on_sale = true;
+	$permalink = $product['link'];
+
 	if($is_on_sale) {
 		$is_on_sale ='<span class="btn-donate">Sale!</span>';		
 	} else {
 		$is_on_sale ='';
 	}
 	return '<div class="col-md-3">
-	        <div class="card">
-	        	'.$image.$is_on_sale.'
-	        	<div class="productInfo clearfix">
-		        	<h3><b>'.$name.'</b></h3>
+	        <div class="card" >
+	        	<a target="_blank" href="'.$permalink.'">
+	        		<img src="'.$image.'" style="width:300px;height:300px" />
+	        		'.$is_on_sale.'
+	        	</a>
+	        	<div class="productInfo clearfix" style="height:137px;">	        		
+		        	<h3><b><a target="_blank" href="'.$permalink.'">'.$name.'</a></b></h3>		        	
 		        	<div class="cancel price leftalign"><strike><small>'.$symbol.$regularPrice.'</small></strike></div>	        	
 		        	<div class="inline-block">
 		        		<div class="price leftalign"><b>'.$symbol.$saleprice.'</b></div>
 		        		<div class="rightalign"><a target="_blank" href="'.$permalink.'" class="iframeBtn">Info</a></div>
 		        	</div>
 	        	</div>
-	        </div>	        
+	        </div>
         </div>';
 }
 
@@ -91,34 +136,34 @@ function wpa3396_page_template( $page_template ) {
     return $page_template;
 }
 
-/**
-* Adding columns for events post type in admin area
-**/
-add_filter( 'manage_pages_columns', 'simple_events_filter_posts_columns' );
-add_filter( 'manage_pages_custom_column', 'simple_events_realestate_column', 10, 2 );
-function simple_events_filter_posts_columns( $columns ) {	
-	// $columns['eventname'] = __( 'Event Name' );
-	// $columns['venu'] = __( 'Venue', 'smashing' );
-	// $columns['location'] = __( 'Location', 'smashing' );
-	// $columns['datetime'] = __( 'Date and Time' );
-	// $columns['shortcode'] = __( 'Shortcode' );
-	$columns['embeded_link'] = __( 'Embeded Link' );
-	return $columns;
-}
+// /**
+// * Adding columns for events post type in admin area
+// **/
+// add_filter( 'manage_pages_columns', 'simple_events_filter_posts_columns' );
+// add_filter( 'manage_pages_custom_column', 'simple_events_realestate_column', 10, 2 );
+// function simple_events_filter_posts_columns( $columns ) {	
+// 	// $columns['eventname'] = __( 'Event Name' );
+// 	// $columns['venu'] = __( 'Venue', 'smashing' );
+// 	// $columns['location'] = __( 'Location', 'smashing' );
+// 	// $columns['datetime'] = __( 'Date and Time' );
+// 	// $columns['shortcode'] = __( 'Shortcode' );
+// 	$columns['embeded_link'] = __( 'Embeded Link' );
+// 	return $columns;
+// }
 
-/**
-* Populating columns for events post type in admin area
-**/
-// add_action( 'manage_simple_events_posts_custom_column', 'simple_events_realestate_column', 10, 2);
-function simple_events_realestate_column( $column, $post_id ) {
-	if ( 'embeded_link' === $column ) {
-		$post_slug = get_post_field( 'post_name', $post_id );
-    	$stringPresent = strpos('-'.$post_slug, 'productembeded');
-    	if ( $stringPresent ) {
-    		echo '&lt;iframe width="100%" style="border: 1px solid #dcdcdc;" height="530px" src="'.get_permalink($post_id).'" frameborder="0"&gt;&lt/iframe&gt;';
-	    }
-	}
-}
+// /**
+// * Populating columns for events post type in admin area
+// **/
+// // add_action( 'manage_simple_events_posts_custom_column', 'simple_events_realestate_column', 10, 2);
+// function simple_events_realestate_column( $column, $post_id ) {
+// 	if ( 'embeded_link' === $column ) {
+// 		$post_slug = get_post_field( 'post_name', $post_id );
+//     	$stringPresent = strpos('-'.$post_slug, 'productembeded');
+//     	if ( $stringPresent ) {
+//     		echo '&lt;iframe width="100%" style="border: 1px solid #dcdcdc;" height="530px" src="'.get_permalink($post_id).'" frameborder="0"&gt;&lt/iframe&gt;';
+// 	    }
+// 	}
+// }
 
 add_action('admin_notices', 'blackhole_tools_admin_notice');
 function blackhole_tools_admin_notice() {
